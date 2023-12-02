e5709ce620700d2968d59d10c46ee43cdc673f04
clhilgert <claytonhilgertmst@gmail.com>
2023-12-02 16:06:00 -0600

    day2 part2

--- day2/index.ts
+++ day2/index.ts
@@ -1,18 +1,22 @@
-// 12 red cubes, 13 green cubes, and 14 blue cubes
-
 import fs from 'fs';
 
-const example1 = 'day2/example1.txt',
-  input = 'day2/input.txt';
+const readFile = (file: string) => {
+  const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
+  return lines;
+};
 
 const partOne = (file: string) => {
-  const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
+  const lines = readFile(file);
   let gameNumber = 1;
   let total = 0;
   const colors: Record<string, number> = { r: 12, g: 13, b: 14 };
 
   lines.map((line) => {
-    const games = line.slice(8).split(';');
+    const colonIndex = line.indexOf(':');
+    const games = line
+      .slice(colonIndex + 1)
+      .trim()
+      .split(';');
     let willSum = true;
     games.forEach((game) => {
       const pick = game.split(',');
@@ -34,4 +38,40 @@ const partOne = (file: string) => {
   console.log(total);
 };
 
-partOne(input);
+const partTwo = (file: string) => {
+  const lines = readFile(file);
+  let total = 0;
+
+  lines.map((line) => {
+    const choice: Record<string, number[]> = {};
+    const colonIndex = line.indexOf(':');
+    const games = line
+      .slice(colonIndex + 1)
+      .trim()
+      .split(';');
+    games.forEach((game) => {
+      const pick = game.split(',');
+      pick.forEach((cube) => {
+        const arr = cube.trim().split('');
+        let pointer = 0;
+        let num = '';
+        while (arr[pointer] !== ' ') {
+          num += arr[pointer++];
+        }
+        const color = arr[++pointer];
+        const number = Number(num);
+        choice[color] = choice[color] ?? [];
+        choice[color].push(number);
+      });
+    });
+    let power = 1;
+    Object.keys(choice).forEach((color) => {
+      let max = Math.max(...choice[color]);
+      power *= max;
+    });
+    total += power;
+  });
+  console.log(total);
+};
+
+partTwo('./day2/input.txt');
