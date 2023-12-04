db9bbb31da736d9567be55f02dfdb4339f8ec249
clhilgert <claytonhilgertmst@gmail.com>
2023-12-04 11:13:44 -0600

    day2 and day4 part 1

--- day2/index.ts
+++ day2/index.ts
@@ -1,10 +1,4 @@
-import fs from 'fs';
 import { getInput } from '../utils';
-
-const readFile = (file: string) => {
-  const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
-  return lines;
-};
 
 const partOne = async () => {
   const lines = await getInput(2);
